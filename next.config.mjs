154b9653786e1a2b8db9882ea3b2009e2b6a67d4
Gustavo Gabriel <gugagabriel56@gmail.com>
2024-09-30 20:10:57 -0300

    build do projeto

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/Projeto-Weather-Infos",
+  output: "export",
+  reactStrictMode: true
+};
 
 export default nextConfig;
